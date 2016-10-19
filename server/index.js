68b7f7260139ec6d5831cce8174f88a8f0cae3dc
prashantmathias <mathiasaudio@gmail.com>
2016-10-19 13:07:03 +0100

    adds CORS support

--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use(function(req, res, next) {
+  res.send('Hello world');
+  next();
+});
+
 mongoose.connect('mongodb://localhost/meanstackapp');
 mongoose.connection.once('open', function() {
   console.log('Listening on port 3000');
