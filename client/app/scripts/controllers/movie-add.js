5c1af1244db366c503db9d957a1fb699742f9a18
prashantmathias <mathiasaudio@gmail.com>
2016-10-21 16:42:25 +0100

    adds navigation view and create movie button

--- client/app/scripts/controllers/movie-add.js
+++ client/app/scripts/controllers/movie-add.js
@@ -11,7 +11,7 @@ angular.module('clientApp')
   .controller('MovieAddCtrl', function ($scope, Movie, $location) {
     $scope.movie = {};
     $scope.saveMovie = function() {
-      Movie.post($scope,movie).then(function() {
+      Movie.post($scope.movie).then(function() {
           $location.path('/movie');
       });
     };
